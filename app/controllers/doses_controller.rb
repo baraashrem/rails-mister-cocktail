10edd060dce53455b413f27a02e25a8654e4d846
Baraa Shrem <baraa.shrem@gmail.com>
2018-11-15 14:37:03 +0200

    cocktails and doses add delete show done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,16 +1,27 @@
 class DosesController < ApplicationController
   before_action :set_cocktail, only: [:new, :create]
+  def index
+    @doses = Dose.all
+  end
+
   def new
     @dose = Dose.new
   end
 
   def create
-    names = params[:dose][:ingredient][1..-1]
-    names.each do |name|
-      ingredient = Ingredient.find_or_create_by(name: name)
-      Dose.create(cocktail: @cocktail, ingredient: ingredient)
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
     end
-  redirect_to @cocktail
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
@@ -19,4 +30,8 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
 
+  resources :doses, only: [:destroy]
+
   resources :cocktails do
     resources :doses
   end
